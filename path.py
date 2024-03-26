9441e4b8a2d0c22e5b5241be6235ab73cf47222c
rickfitch <rickfitch@hotmail.com>
2024-03-25 19:56:48 -0700

    Add new files and update git cheatsheet

--- path.py
+++ path.py
@@ -3,7 +3,7 @@ from pathlib import Path
 path = Path("git_cheatsheet.txt")
 
 if(path.exists()):
-    content = path.read_text()
+    content = path.read_text()  
     print(content.upper())
 else:
     print("File does not exist")
